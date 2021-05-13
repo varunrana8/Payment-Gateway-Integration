bc3cc007a5b741df4695803abc501fce23a2d5ab
Varun <54104000+varunrana8@users.noreply.github.com>
2021-05-13 20:36:54 +0530

    Update info.php

--- info.php
+++ info.php
@@ -1,21 +1,26 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "officialpurposeforproject@gmail.com";
-    $name = $_POST['name'];
-    $from = $_POST['Email'];
-    $subject = "Form submission";
-    $message = $_POST['Message'];
-    $headers = "From:" . $from;
-    
-    if (empty($name) || empty($from) || empty($message))
-    {
-        echo "Please Fill all the details";
-    }
-    else
-    {
-        mail($to,$subject,$message,$headers);
-        echo "<script type='text/javascript'>alert('Message sent Successfully');window.history.go(-1);
+    $mail = new PHPMailer();
+
+// Settings
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+
+$mail->Host       = "mail.example.com"; // SMTP server example
+$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "officialpurposeforproject@gmail.com"; // SMTP account username example
+$mail->Password   = "8800583044";        // SMTP account password example
+
+// Content
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+echo "<script type='text/javascript'>alert('Message sent Successfully')
         </script>";
-    }
 }
 ?>
